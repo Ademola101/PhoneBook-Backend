f3768da2da5fab524dca148c14889ab1db0d031b
Ademola <ademolaogunmokun@yahoo.com>
2022-06-22 00:27:08 +0100

    command

--- mongoose.js
+++ mongoose.js
@@ -15,6 +15,23 @@ const PhoneBook = mongoose.model("PhoneBook", PhonebookSchema);
 
 mongoose.connect(url).then(result => {
   console.log("connected");
+  if(process.argv.length < 4){
+    PhoneBook.find({}).then(results => {
+      results.forEach(result => {
+        console.log(result);
+      })
+    })
+  }
+  const person = new PhoneBook({
+    name: process.argv[3],
+    number: process.argv[4]
+  });
+  return person.save()
+}).then(result => {
+  console.log(`added ${process.argv[3]} number ${process.argv[4]}`);
+   return mongoose.connection.close()
+}).catch(error => {
+  console.log(error);
 })
 
 
