de079c92b400b2190c3e9867d6c7d153cbba1e88
Ademola <ademolaogunmokun@yahoo.com>
2022-06-22 09:41:30 +0100

    Phonebook model

---
+++ models/Phoneboook.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const url  = process.env.MONGODB_URL
+
+console.log("connecting to", url);
+
+mongoose.connect(url).then(result => {
+  console.log("connected to db");
+}).catch(error => {
+  console.log("error connecting to db", error.message);
+});
+
+const Schema = mongoose.Schema;
+const PhonebookSchema = new Schema({
+  name: String,
+  number: Number
+});
+
+PhonebookSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+});
+
+module.exports = mongoose.model("PhoneBook", PhonebookSchema);
